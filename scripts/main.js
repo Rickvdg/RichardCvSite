c76ca858a6bba4b65e4469703d94f872a0ad28ec
RickVanDerGun <rickvdg97@gmail.com>
2025-11-11 16:15:24 +0100

    Update responsive styles for split-container and navbar

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-// Only keep navbar scroll effect
-window.addEventListener('scroll', () => {
-    const navbar = document.querySelector('.navbar');
-    if (window.scrollY > 50) {
-        navbar.classList.add('scrolled');
-    } else {
-        navbar.classList.remove('scrolled');
-    }
-});
